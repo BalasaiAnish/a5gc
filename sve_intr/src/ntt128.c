3f91d384ca164016039599aa8d8a0e78ab8501fa
BalasaiAnish <ponnaluribalasai@gmail.com>
2026-01-09 18:53:49 +0530

    final changes

--- sve_intr/src/ntt128.c
+++
@@ -1,8 +0,0 @@
-#include "ntt128.h"
-
-void ntt_sve128(int16_t r[256], const int16_t zetas[128]) {
-    level0_sve128(r,zetas,0);
-    level0_sve128(r,zetas,1);
-    levels1t6_sve128(r,zetas,0);
-    levels1t6_sve128(r,zetas,1);
-}
--- sve_intr/test/default.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Default test\n");
-}
